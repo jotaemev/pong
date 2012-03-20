96607f7a5132438333ca66d11c647ab9cef1eae4
Daniel Sanchez <3danimanimal@gmail.com>
2012-03-20 14:46:28 +0100

    loop updated, not working already

--- src/main.scm
+++ src/main.scm
@@ -9,16 +9,48 @@
                     (canvas (graphics-canvas graphics))
                     (maxx (environment-size-x env))
                     (maxy (environment-size-y env))
-                    (box-size-x (/ (* maxx 2.5) 100))
-                    (box-size-y (* maxy 20))
-                    (box-1-pos-x (/ (* maxx 10) 100))
-                    (box-2-pos-x (- maxx (/ (* maxx 10) 100))))
-               (let loop ((box-1-pos-y (/ maxy 2))
-                          (box-2-pos-y (/ maxy 2)))
+                    (box-size-x (/ (* maxx 1) 100))
+                    (box-size-y (/ (* maxy 20) 100))
+                    (box-1-pos-x (/ (* maxx 5) 100))
+                    (box-2-pos-x (- maxx (/ (* maxx 5) 100)))
+                    (c-pos-x (/ (* maxx 50) 100))
+                    (c-pos-y (/ (* maxy 50) 100))
+                    (c-size 10))
+               (let loop ((box-1-pos-y (/ (* maxy 50) 100))
+                          (box-2-pos-y (/ (* maxy 50) 100)))
                  (draw:fill-color! (make-color/rgba 1 1 1 1))
-                 ;(draw:rectangle/corners 0 0 (+ 0  box-size-x) (+ 0 box-size-y))
-                 ;(draw:rectangle/corners 0 0 10 10)
-                 (draw:rectangle/center box-1-pos-x box-1-pos-y (/ box-size-x 10) (/ box-size-x 10))
-                 ;(draw:rectangle/center box-2-pos-x box-2-pos-y box-size-x box-size-y)
+                 (draw:rectangle/center-sides  box-1-pos-x box-1-pos-y box-size-x box-size-y)
+                 (draw:rectangle/center-sides  box-2-pos-x box-2-pos-y box-size-x box-size-y) 
+
+                 #;
+                 (cond 
+                  ((input:key-pressed? 2) (set! box-2-pos-y (+ box-2-pos-y 10)))
+                  ((input:key-pressed? 3) (set! box-2-pos-y (- box-2-pos-y 10))))
+                 
+
+                 #;
+                 (cond 
+                 ((input:key-pressed? 119) (+ box-1-pos-y 10))
+                 ((input:key-pressed? 115) (- box-1-pos-y 10)))
+
+
+                 (draw:circle/center c-pos-x c-pos-y c-size)
                  (draw:on surface)
-                 (loop box-1-pos-y box-2-pos-y)))))
+                 (loop 
+                  
+                  (cond 
+                   ((input:key-pressed? 119) (set! box-1-pos-y (+ box-2-pos-y 10)))
+                   ((input:key-pressed? 115) (set! box-1-pos-y (- box-2-pos-y 10)))
+                   (else
+                    (box-1-pos-y)))
+
+
+
+                  (cond 
+                   ((input:key-pressed? 2) (set! box-2-pos-y (+ box-2-pos-y 10)))
+                   ((input:key-pressed? 3) (set! box-2-pos-y (- box-2-pos-y 10)))
+                   (else
+                    (box-2-pos-y)))
+                  
+
+                  )))))
