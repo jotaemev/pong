6380e4c5e1542151324b5608a3875bcfb1a46a69
Daniel Sanchez <3danimanimal@gmail.com>
2012-03-20 17:44:01 +0100

    Reestructured and commented

--- src/main.scm
+++ src/main.scm
@@ -18,25 +18,28 @@
                     (c-size 10))
                (let loop ((box-1-pos-y (/ (* maxy 50) 100))
                           (box-2-pos-y (/ (* maxy 50) 100)))
+                 ; Draw neutral canvas
+                 (draw:fill-color! (make-color/rgba 0 0 0 1))
+                 (draw:rectangle/corner-corner 0 0 maxx maxy)
+                 
+                 
+                 ;Draw players
                  (draw:fill-color! (make-color/rgba 1 1 1 1))
                  (draw:rectangle/center-sides  box-1-pos-x box-1-pos-y box-size-x box-size-y)
                  (draw:rectangle/center-sides  box-2-pos-x box-2-pos-y box-size-x box-size-y) 
 
-                 #;
-                 (cond 
-                  ((input:key-pressed? 2) (set! box-2-pos-y (+ box-2-pos-y 10)))
-                  ((input:key-pressed? 3) (set! box-2-pos-y (- box-2-pos-y 10))))
+
                  
 
-                 #;
-                 (cond 
-                 ((input:key-pressed? 119) (+ box-1-pos-y 10))
-                 ((input:key-pressed? 115) (- box-1-pos-y 10)))
 
 
+                 ;Draw ball
                  (draw:circle/center c-pos-x c-pos-y c-size)
                  (draw:on surface)
-                 (loop #;(+ box-1-pos-y 1)  #;(+ box-2-pos-y 1)  
+                 
+                 
+                 ;Main loop
+                 (loop  
                   
                   
                   (cond 
