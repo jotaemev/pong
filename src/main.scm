97d702c74dcb38da84b876d173b3838aef8e5a2b
Daniel Sanchez <3danimanimal@gmail.com>
2012-03-21 14:56:03 +0100

    refreshing image added

--- src/main.scm
+++ src/main.scm
@@ -1,8 +1,8 @@
 ;; Copyright (c) 2012, Alvaro Castro-Castilla. All rights reserved.
-;; Game by: Daniel Sanchez, Alejandro Cabeza, Carlos Belmonte and Juan-Maria Vergara
+;; Game by: Daniel Sanchez, Alejandro Cabeza, Carlos Belmonte and Juan Maria Vergara
 (pg:app
  setup: (lambda (resources)
-          (make-environment 1280 800 resources #f))
+          (make-environment 600 400 resources #f))
  main-loop: (lambda (env)
              (let* ((graphics (environment-graphics env))
                     (surface (graphics-surface graphics))
@@ -15,12 +15,14 @@
                     (box-2-pos-x (- maxx (/ (* maxx 5) 100)))
                     (max-pos-y (- maxy box-size-y))
                     (min-pos-y box-size-y)
-                    (c-pos-x (/ (* maxx 50) 100))
-                    (c-pos-y (/ (* maxy 50) 100))
-                    (c-size 10))
+                    (init-c-pos-x (/ (* maxx 50) 100))
+                    (init-c-pos-y (/ (* maxy 50) 100))
+                    (c-size (/ (* maxy 1) 100)))
                (let loop ((box-1-pos-y (/ (* maxy 50) 100))
-                          (box-2-pos-y (/ (* maxy 50) 100)))
-                 ; Draw neutral canvas
+                          (box-2-pos-y (/ (* maxy 50) 100))
+                          (c-pos-x init-c-pos-x)
+                          (c-pos-y init-c-pos-y))
+                 ;Draw neutral canvas
                  (draw:fill-color! (make-color/rgba 0 0 0 1))
                  (draw:rectangle/corner-corner 0 0 maxx maxy)
                  
@@ -29,7 +31,7 @@
                  (draw:fill-color! (make-color/rgba 1 1 1 1))
                  (draw:rectangle/center-sides  box-1-pos-x box-1-pos-y box-size-x box-size-y)
                  (draw:rectangle/center-sides  box-2-pos-x box-2-pos-y box-size-x box-size-y) 
-
+                 
 
                  
 
@@ -38,7 +40,10 @@
                  ;Draw ball
                  (draw:circle/center c-pos-x c-pos-y c-size)
                  (draw:on surface)
-                 
+
+                 ;EXIT
+                 (if (input:key-pressed? 27)
+                      (exit 0))
                  
                  ;Main loop
                  (loop  
@@ -46,22 +51,29 @@
                   ;Player One box movement
                   (cond 
                    ((input:key-pressed? 119) (if (< (- box-1-pos-y 10) min-pos-y) min-pos-y
-                                                 (- box-1-pos-y 10)))
+                                                 (- box-1-pos-y 0.5)))
                    
                    ((input:key-pressed? 115) (if (> (+ box-1-pos-y 10) max-pos-y) max-pos-y
-                                                 (+ box-1-pos-y 10)))
+                                                 (+ box-1-pos-y 0.5)))
                    (else
                     box-1-pos-y))
 
                   ;Player Two box movement 
                   (cond 
                    ((input:key-pressed? 273) (if (< (- box-2-pos-y 10) min-pos-y) min-pos-y
-                                                 (- box-2-pos-y 10)))
+                                                 (- box-2-pos-y 0.5)))
                    
                    ((input:key-pressed? 274) (if (> (+ box-2-pos-y 10) max-pos-y) max-pos-y
-                                                 (+ box-2-pos-y 10)))
+                                                 (+ box-2-pos-y 0.5)))
                    (else
                     box-2-pos-y))
+
+
+                  ;Ball, circle, movement -X
+                  ;(cond)
+                  (+ c-pos-x 0.05)
+                  ;Ball, circle, movement -Y
+                  (+ c-pos-y 0.05)
                   
 
                   )))))
